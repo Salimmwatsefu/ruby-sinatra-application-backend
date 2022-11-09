82ba6deee67590b5340ad00ccf7c5cb0b8f9ddbd
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-09 15:45:57 +0300

    seed the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,57 @@
-# This will delete any existing rows from the Product and User tables
-# so you can run the seed file multiple times without having duplicate entries in your database
-puts "Deleting old data..."
-Product.destroy_all
-
 puts "🌱 Seeding data..."
 
 # place your seeds here
+Product.create(name:"Sayona Tv",category:"Televisions",description:"This is the Newest wide range of LED  from WEYON electronic. WEYON electronic launched this new technology as a means of opening a new era for smartphone and the Internet products which means the company brings together the smart technologies into smart TV so as to offer a new seamless way to enjoy media.
+
+    synchronously every day.5000+ TV applications, explore infinite entertainment options.image 
+    
+    It has very clear natural realistic pictures, you will fall in love with 32 Inch 80cm HD LED TV 32 will redefine the way you enjoy entertainment, the slim LED TV will be a perfect addition to electronics collection. This WEYON TV will allow you to see images in a sharp and crystal clear form. With a high resolution, you will be able to see colors in their true form. This will make you have an enjoyable TV experience. Entertainment gets better when you have a very good television. 
+    
+    Smooth running speed
+    
+    The WEYON TV series also offers 60Hz frame rate .This allow viewers to enjoy a smoother motion and more detail in movement without blur.If you are a lover of sport and movies that involves a lot of fast-moving pictures, then this LED TV is the right choice. In addition,it has an in-built with technology that will eliminate blur from fast-moving pictures. This will ensure you have complete all-around entertainment. image
+    
+    picture quality
+    
+    Smart distributes the LED lights on the back of the panel, so that the backlight can be evenly transmitted to the entire screen, the brightness and color gamut are guaranteed, the backlight rendering image is more accurate, and the picture details are more delicate and realistic. D-LED technology allows you to go beyond the screen into a fully immersive world.When you watch a movie with your family, it can give you an immersive experience.",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/64/621176/1.jpg?0263",price:12497)
+
+    Product.create(name:"LG 75NANO75 75 nanocell",category:"Televisions",description:"Get the full range of enjoyment.
+        LG’s own dynamic range technology, HDR 10 Pro, adjusts brightness to enhance color, reveal every tiny detail, and bring lifelike clarity to every image — it also intensifies regular HDR content. Now all your favorite movies and shows will be more vivid and vibrant from beginning to end.",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/21/452959/1.jpg?6791",price:196000)
+    Product.create(name:"SAMSUNG",category:"Televisions",description:"Brilliant, long-lasting color with Quantum Dot
+        Wider range of contrast in cinematic scale
+        A smarter, faster 4K processor
+        HDR 10+
+        Resolutions: 3840 x 2160
+        Dolby Digital Plus
+        WHAT’S IN THE BOX
+        Samsung 55 Qled UHD 4K
+        Remote Control
+        Power cable 
+        User Manual
+        Table-Top Stand
+        SPECIFICATIONS
+        SKU: SA948EA0DKMS8NAFAMZ
+        Model: 55Q60BAU
+        Production Country: Egypt
+        Size (L x W x H cm): 9.0
+        Weight (kg): 9
+        Main Material: PVC
+        Care Label: Handle with Caution",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/22/797656/2.jpg?3815",price:105899)
+
+    Product.create(name:"Vitron",category:"Televisions",description:"If you are looking for an all-in one TV that saves power at the same time, then VITRON 4368FS 43 Smart is your BEST choice. It has Full High Definition (HD) video resolution and WiFi-enabled making it amazing for movie lovers who prefer streaming through popular Apps like YouTube and Netflix. Running on android 9.0, our Smart Module allows you to enjoy a customized browser and enjoy Optimized Connectivity  with 2 USB ports and 3 HDMI ports. With Vision Plus care warranty we have you covered for 14 Months
 
 
+    Connectivity
+    VITRON 43″ TV has a powerful stream media that helps you read the audio and video files in your mobile devices at anytime. You can also browse your fascinating photos while enjoying music. Our TV has powerful identification and reading functions, providing much better choice for you.",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/64/434378/1.jpg?0797",price:21800)
+    Product.create(name:"vitron Tv",category:"Televisions",description:"",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/08/017613/1.jpg?9012",price:13194)
+    Product.create(name:"TCL",category:"Televisions",description:"",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/70/241634/1.jpg?9347",price:59000)
+    Product.create(name:"Hisense Tv",category:"Televisions",description:"HISENSE 43A62GS
+        Immerse yourself in breathtaking 4K HDR detail with the Hisense A62GS, Smart  4K Ultra HD resolution delivers pin-sharp picture quality whilst HDR maximises contrast and colour accuracy. UHD AI Upscaler enhances all of your favourite sports, TV shows and movies to super clear, near-4K quality effortlessly. Hisense's VIDAA U Smart TV OS lets you enjoy all your favourite entertainment from Netflix, Prime Video, Freeview Play and more, and control your TV using voice with Alexa built-in. Order today from Jumia Kenya and have delivered at your door step",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/01/467394/1.jpg?0742",price:39000)
+    Product.create(name:"TCL",category:"Televisions",description:"4 microphone receivers ensure exceptional reception and range, so you can control your content totally hands-free. Watch movies, ask questions, and play music with simple voice requests. Whether enjoying family time, or sweating it out with online exercise routines, don't let the remote get in the way – just say it, and you'll see it.",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/70/701784/3.jpg?7667",price:42000)
+    Product.create(name:"Hisense",category:"Televisions",description:"Hisense 55A62GS
+
+        Hisense 85 smart 4k Frameless  55A62GS Sizes 55 Inches HDR Dolby Vision HDR & HDR10 Connectivity Wireless, Bluetooth Other Features DTS Virtual, full-array backlight, wall mountable, voice remote Quantum Dot,
+        
+        Order today at Jumia Kenya and have delivered at your door step",image:"https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/98/090626/1.jpg?1638",price:73000)
 
 puts "🌱 Done seeding!"
